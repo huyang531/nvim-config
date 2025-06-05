74dde5b6439fbb368bb03f1b6f191cfda68ec2bf
Yang Hu <yanghuu531@gmail.com>
2025-06-05 16:57:40 -0400

    add lldb config for macOS

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -8,6 +8,9 @@ return {
 		config = function()
 			local dap, dapui = require("dap"), require("dapui")
 
+			-- setup dap log (use INFO in normal cases, DEBUG or TRACE for debugging)
+			dap.set_log_level("INFO")
+
 			-- setup dap keymap
 			vim.keymap.set("n", "<F5>", dap.continue, {})
 			vim.keymap.set("n", "<F10>", dap.step_over, {})
@@ -51,12 +54,90 @@ return {
 				dapui.close()
 			end
 
-			-- setup cpp debugger (lldb)
-			dap.configuration.cpp = {
+			-- setup cpp/c/rust debugger (lldb)
+			dap.adapters.lldb = {
+				type = "executable",
+				command = "/Applications/Xcode.app/Contents/Developer/usr/bin/lldb-dap",
+				name = "lldb",
+			}
+
+			dap.configurations.cpp = {
 				{
 					name = "Launch",
 					type = "lldb",
 					request = "launch",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					end,
+					cwd = "${workspaceFolder}",
+					stopOnEntry = false,
+					args = {},
+					env = function()
+						local variables = {}
+						for k, v in pairs(vim.fn.environ()) do
+							table.insert(variables, string.format("%s=%s", k, v))
+						end
+						return variables
+					end,
+				},
+			}
+
+			dap.configurations.c = dap.configurations.cpp
+
+			dap.configurations.rust = {
+				{
+					name = "Launch",
+					type = "lldb",
+					request = "launch",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					end,
+					cwd = "${workspaceFolder}",
+					stopOnEntry = false,
+					args = {},
+					initCommands = function()
+						-- Find out where to look for the pretty printer Python module.
+						local rustc_sysroot = vim.fn.trim(vim.fn.system("rustc --print sysroot"))
+						assert(
+							vim.v.shell_error == 0,
+							"failed to get rust sysroot using `rustc --print sysroot`: " .. rustc_sysroot
+						)
+						local script_file = rustc_sysroot .. "/lib/rustlib/etc/lldb_lookup.py"
+						local commands_file = rustc_sysroot .. "/lib/rustlib/etc/lldb_commands"
+
+						-- The following is a table/list of lldb commands, which have a syntax
+						-- similar to shell commands.
+						--
+						-- To see which command options are supported, you can run these commands
+						-- in a shell:
+						--
+						--   * lldb --batch -o 'help command script import'
+						--   * lldb --batch -o 'help command source'
+						--
+						-- Commands prefixed with `?` are quiet on success (nothing is written to
+						-- debugger console if the command succeeds).
+						--
+						-- Prefixing a command with `!` enables error checking (if a command
+						-- prefixed with `!` fails, subsequent commands will not be run).
+						--
+						-- NOTE: it is possible to put these commands inside the ~/.lldbinit
+						-- config file instead, which would enable rust types globally for ALL
+						-- lldb sessions (i.e. including those run outside of nvim). However,
+						-- that may lead to conflicts when debugging other languages, as the type
+						-- formatters are merely regex-matched against type names. Also note that
+						-- .lldbinit doesn't support the `!` and `?` prefix shorthands.
+						return {
+							([[!command script import '%s']]):format(script_file),
+							([[command source '%s']]):format(commands_file),
+						}
+					end,
+					env = function()
+						local variables = {}
+						for k, v in pairs(vim.fn.environ()) do
+							table.insert(variables, string.format("%s=%s", k, v))
+						end
+						return variables
+					end,
 				},
 			}
 
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,34 +1,41 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	name = "neo-tree",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-		-- {"3rd/image.nvim", opts = {}}, – Optional image support in preview window: See `# Preview Mode` for more information
-	},
-	lazy = false, -- neo-tree will lazily load itself
-	---@module "neo-tree"
-	---@type neotree.Config?
-	opts = {
-		-- fill any relevant options here
-	},
-	config = function()
-		require("neo-tree").setup({
-			filesystem = {
-				follow_current_file = {
-					enabled = true,
-					leave_dirs_open = true,
-				},
-			},
-		})
-		vim.keymap.set(
-			"n",
-			"<leader>fe",
-			":Neotree filesystem reveal left<CR>",
-			{ desc = "Open Neotree Filesystem on the Left", noremap = true, silent = true }
-		)
-		-- vim.keymap.set('n', '<leader>neo', ':Neotree focus<CR>', { desc = "Focus on Neotree" })
-	end,
+    "nvim-neo-tree/neo-tree.nvim",
+    name = "neo-tree",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+        -- {"3rd/image.nvim", opts = {}}, – Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    ---@module "neo-tree"
+    ---@type neotree.Config?
+    opts = {
+        -- fill any relevant options here
+    },
+    config = function()
+        require("neo-tree").setup({
+            filesystem = {
+                bind_to_cwd = true,
+                follow_current_file = {
+                    enabled = true,
+                    leave_dirs_open = true,
+                },
+            },
+        })
+        -- vim.keymap.set(
+        -- 			"n",
+        -- 			"<leader>fe",
+        -- 			":Neotree filesystem reveal left<CR>",
+        -- 			{ desc = "Open Neotree Filesystem on the Left", noremap = true, silent = true }
+        -- 		)
+        vim.keymap.set(
+            "n",
+            "<leader>fe",
+            ":Neotree toggle<CR>",
+            { desc = "Toggle Neotree Filesystem on the Left", noremap = true, silent = true }
+        )
+        -- vim.keymap.set('n', '<leader>neo', ':Neotree focus<CR>', { desc = "Focus on Neotree" })
+    end,
 }
